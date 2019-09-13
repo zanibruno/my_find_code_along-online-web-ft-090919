dbaab584fd31e57b374304978c5f7e803889d092
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-13 19:24:23 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,9 @@ require 'pry'
 
 def my_find(collection)
 counter = 0 
-while counter < collection.length 
-return collection[counter] if yield(collection[counter])
+ while counter < collection.length 
+  return collection[counter] 
+   if yield(collection[counter])
 counter += 1 
-end
+ end
 end
